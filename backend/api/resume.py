541510528ec20e55e4e86d7e6aa104b7725f8988
Ritesh Pathak <iamriteshpathak1994@gmail.com>
2026-07-11 14:09:10 +0530

    Sprint 2: Added pdf validation for resume upload

--- backend/api/resume.py
+++ backend/api/resume.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, UploadFile, File
+import os
+from fastapi import APIRouter, UploadFile, File, HTTPException
 
 router = APIRouter(
     prefix="/resume",
@@ -14,6 +15,20 @@ def test_resume():
 async def upload_resume(
     file: UploadFile = File(...)
 ):
+
+    if not file.filename.lower().endswith(".pdf"):
+        raise HTTPException(
+            status_code=400,
+            details = "Only Pdf files are allowed"
+        )
+
+    os.makedirs("uploads", exist_ok=True)
+
+    contents = await file.read()
+
+    with open(f"uploads/{file.filename}","wb") as f:
+        f.write(contents)
     return {
+        "message": "Resume uploaded successfully",
         "filename": file.filename
     }
